9b02025f5bbe708988f98c24a26049f441347926
Christian Bolus <christianjbolus@gmail.com>
2021-06-28 14:26:11 -0400

    Add restrict middleware

---
+++ helpers/restrict.js
@@ -0,0 +1,14 @@
+import jwt from 'jsonwebtoken';
+
+const TOKEN_KEY = process.env.TOKEN_KEY || 'notagoodkey';
+
+export const restrict = async (req, res, next) => {
+  try {
+    const token = req.headers.authorization.split(' ')[1];
+    jwt.verify(token, TOKEN_KEY);
+    next()
+  } catch (error) {
+    console.log(error)
+    res.status(403).send('Not Authorized')
+  }
+};
