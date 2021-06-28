551e1f9495281e82467095be583c8c74374b2294
Christian Bolus <christianjbolus@gmail.com>
2021-06-28 14:12:07 -0400

    Set up db connection

--- controllers/users.js
+++ controllers/users.js
@@ -59,31 +59,30 @@ export const signIn = async (req, res) => {
 
 export const verify = async (req, res) => {
   try {
-    const token = req.headers.authorication.split(' ')[1]
-    const payload = jwt.verify(token, TOKEN_KEY)
+    const token = req.headers.authorication.split(' ')[1];
+    const payload = jwt.verify(token, TOKEN_KEY);
     if (payload) {
-      res.json(payload)
+      res.json(payload);
     }
   } catch (error) {
-    console.log(error.message)
-    res.status(401).send('Nout Authorized')
+    console.log(error.message);
+    res.status(401).send('Nout Authorized');
   }
-}
+};
 
 export const changePassword = async (req, res) => {
   try {
-    const {email, password, newPassword} = req.body;
-    const user = await User.findOne({email: email}).select('username email password_digest')
+    const { email, password, newPassword } = req.body;
+    const user = await User.findOne({ email: email }).select('username email password_digest');
     if (await bcrypt.compare(password, user.password_digest)) {
-      user.password_digest = await bcrypt.hash(newPassword, SALT_ROUNDS)
-      await user.save()
-      res.status(200).send('Password change successful')
+      user.password_digest = await bcrypt.hash(newPassword, SALT_ROUNDS);
+      await user.save();
+      res.status(200).send('Password change successful');
     } else {
-      res.status(401).send('Invalid Credentials')
+      res.status(401).send('Invalid Credentials');
     }
   } catch (error) {
-    console.log(error.message)
-    res.status(500).send('Invalid Credentails')
+    console.log(error.message);
+    res.status(500).send('Invalid Credentails');
   }
-}
-
+};
