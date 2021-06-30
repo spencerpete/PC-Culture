b3a3f1b49fa6b99b5b4b2c2d4fe602f9b32685b3
Spencer Peterson <spencerpeterson@MacBook-Pro-4.lan>
2021-06-30 11:38:13 -0400

    add landing page carousel

--- client/src/screens/Home/Home.jsx
+++ client/src/screens/Home/Home.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Carousel from '../../components/Carousel/Carousel';
 import CategoryCard from '../../components/CategoryCard/CategoryCard';
 import Layout from '../../components/Layout/Layout';
 
@@ -7,7 +8,7 @@ export default function Home() {
     <Layout>
       <div>
         <div>
-          <img src="https://images.unsplash.com/photo-1587202372634-32705e3bf49c?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80" />
+          <Carousel />
         </div>
         <div className="landingPage-copy-container">
           <div className="copy-title">What make us diff</div>
