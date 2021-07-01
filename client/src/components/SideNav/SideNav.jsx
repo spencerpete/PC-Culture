22e413bd416c08acd2fb3a742f031a9e2a53d531
Christian Bolus <christianjbolus@gmail.com>
2021-07-01 12:00:40 -0400

    Add initial styling to SideNav

--- client/src/components/SideNav/SideNav.jsx
+++ client/src/components/SideNav/SideNav.jsx
@@ -1,23 +1,27 @@
 import { Link } from 'react-router-dom';
+import { FaTimes } from 'react-icons/fa';
+import './SideNav.css'
 
 const SideNav = ({ user, toggleShow }) => {
   return (
-    <div className="sidenav-container">
-      <div className="side-nav">
-        <div className="sidenav-header">
-          {/* icon */}
-          <h2 className="sidenav-brand">PC Culture</h2>
+    <div className="sidenav-container bg-black bg-opacity-50 fixed w-full h-full">
+      <div className="side-nav flex flex-col w-2/4 fixed h-full bg-white">
+        <div className=" sidenav-header flex items-center justify-center h-12 bg-black text-white text-center mb-10">
+          <FaTimes className="absolute left-1 top-1"/>
+          <h2 className="sidenav-brand text-2xl font-semibold">PC Culture</h2>
         </div>
-        <Link path="/products">All Parts</Link>
-        <Link path="/products/new">Create Part</Link>
-        {user ? (
-          <Link path="/sign-out">Sign Out</Link>
-        ) : (
-          <>
-            <Link path="/sign-in">Sign In</Link>
-            <Link path="/sign-up">Sign Up</Link>
-          </>
-        )}
+        <div className="sidenav-links flex flex-col items-center">
+          <Link className="sidenav-link" path="/products">All Parts</Link>
+          <Link className="sidenav-link" path="/products/new">Create Part</Link>
+          {user ? (
+            <Link className="sidenav-link" path="/sign-out">Sign Out</Link>
+          ) : (
+            <>
+              <Link className="sidenav-link" path="/sign-in">Sign In</Link>
+              <Link className="sidenav-link" path="/sign-up">Sign Up</Link>
+            </>
+          )}
+        </div>
       </div>
     </div>
   );
