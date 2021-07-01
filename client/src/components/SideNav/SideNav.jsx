547cd090677ecd5bfe9b082ed7eb454b1471e794
Christian Bolus <christianjbolus@gmail.com>
2021-07-01 15:21:31 -0400

    Reorganize SideNav CSS

--- client/src/components/SideNav/SideNav.jsx
+++ client/src/components/SideNav/SideNav.jsx
@@ -6,11 +6,11 @@ const SideNav = ({ user, show, toggleShow }) => {
   return (
     <div className={show ? "sidenav-container show" : "sidenav-container"}>
       <div className={show ? "sidenav show" : "sidenav"}>
-        <div className=" sidenav-header flex items-center justify-center h-12 bg-black text-white text-center mb-10">
-          <FaTimes className="absolute left-1 top-1" onClick={toggleShow}/>
-          <h2 className="sidenav-brand text-2xl font-semibold">PC Culture</h2>
+        <div className=" sidenav-header">
+          <FaTimes className="close" onClick={toggleShow}/>
+          <h2 className="sidenav-brand">PC Culture</h2>
         </div>
-        <div className="sidenav-links flex flex-col items-center">
+        <div className="sidenav-links">
           <Link className="sidenav-link" to="/products">All Parts</Link>
           <Link className="sidenav-link" to="/products/new">Create Part</Link>
           {user ? (
