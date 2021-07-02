eab1ea7331b4c595a8a18cd9f4187e81da3aed41
James <james@jamess-mbp.home>
2021-07-02 10:15:04 -0500

    CSS for productCard finished

--- client/src/components/ProductCard/ProductCard.jsx
+++ client/src/components/ProductCard/ProductCard.jsx
@@ -3,9 +3,9 @@ import Button from '../Button/Button'
 
 const ProductCard = props => {
   return (
-    <div className="bg-gray-50">
-      <div className="detailImg rounded-3xl shadow-lg m-3 mb-8 h-52">
-        <img className="m-auto rounded-3xl object-cover h-52 w-full" src={props.product.imgURL} alt={props.product.name} />
+    <div>
+      <div className="detailImg bg-white rounded-3xl shadow-lg m-3 mb-8 h-80 overflow-hidden">
+        <img className="object-center rounded-3xl object-contain h-96 w-full overflow-hidden" src={props.product.imgURL} alt={props.product.name} />
       </div>
       <div className="rounded-3xl shadow-lg m-3 text-center bg-white mt-11">
         <div className="detailName m-3 text-2xl font-semibold p-3 pt-10">{props.product.name}</div>
@@ -19,3 +19,5 @@ const ProductCard = props => {
   );
 };
 export default ProductCard;
+
+
