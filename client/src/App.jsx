792d171bd40a140ef0baab9421e23c001ab4a9dd
Spencer Peterson <spencerpeterson@MacBook-Pro-4.lan>
2021-07-02 14:41:50 -0400

    import signout

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,6 +7,7 @@ import ProductEdit from './screens/ProductEdit/ProductEdit';
 import Home from './screens/Home/Home';
 import SignUpIn from './screens/SignUp-In/SignUp-In';
 import ProductDetail from './screens/ProductDetail/ProductDetail';
+import SignOut from './screens/SignOut/SignOut';
 import './App.css';
 
 const App = () => {
