45eb7b8e3d1331d767976fd2154a4b1d9eadb900
Christian Bolus <christianjbolus@gmail.com>
2021-07-06 14:03:44 -0400

    Update homepage copy and fix navbar scrolling bug

--- client/src/screens/Home/Home.jsx
+++ client/src/screens/Home/Home.jsx
@@ -12,12 +12,12 @@ export default function Home({ user }) {
           <Carousel />
         </div>
         <div className="landingPage-copy-container">
-          <div className="copy-title">What make us diff</div>
+          <div className="copy-title">About Us</div>
           <div className="copy-body">
-            Welcome to PC Culture! Your one stop for all your pc building needs. We got graphics
-            cards, motherboards, CPUs, SSDs... you want it, we got it, and don't worry we have gone
+            Welcome to PC Culture! Your one stop for all your PC building needs. Whether it's graphics
+            cards, motherboards, CPUs, SSDs... you want it, we got it. And don't worry, we have gone
             through and picked out the best of the best so no matter your PC knowledge you can rest
-            knowing that no matter the build you are going to have one spanking hot PC!
+            easy knowing that no matter the build you are going to have one spanking hot PC!
           </div>
         </div>
         <CategoryCard />
