d6dd3d192aff4a308286b3ae2af12b91709161cf
Christian Bolus <christianjbolus@gmail.com>
2021-07-06 20:16:50 -0400

    Reorganize routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -27,13 +27,13 @@ const App = () => {
         <Route exact path="/">
           <Home user={user} />
         </Route>
-        <Route exact path="/products/:category">
-          <ProductList user={user} />
-        </Route>
         <Route exact path="/products/new">
           {user ? <ProductCreate user={user} /> : <Redirect to="/sign-up" />}
         </Route>
-        <Route exact path="/products/:id">
+        <Route exact path="/products/:category">
+          <ProductList user={user} />
+        </Route>
+        <Route exact path="/product/:id">
           <ProductDetail user={user} />
         </Route>
         <Route exact path="/products/:id/edit">
